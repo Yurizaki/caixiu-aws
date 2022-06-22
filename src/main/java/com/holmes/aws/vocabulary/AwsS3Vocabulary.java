aeaaef2efcc07ceeb7b2dd36db7f49762379f171
Alex <alxdev94@gmail.com>
2022-06-22 23:29:08 +0100

    get and put object

--- src/main/java/com/holmes/aws/vocabulary/AwsS3Vocabulary.java
+++ src/main/java/com/holmes/aws/vocabulary/AwsS3Vocabulary.java
@@ -1,60 +1,50 @@
 package com.holmes.aws.vocabulary;
 
+import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.io.OutputStream;
+import java.nio.file.Files;
 
 public class AwsS3Vocabulary {
 
     private final S3Client s3Client;
+    private final String bucketName;
 
-    public AwsS3Vocabulary(S3Client s3Client) throws IOException {
+    public AwsS3Vocabulary(S3Client s3Client, String bucketName)  {
         this.s3Client = s3Client;
-
-
-        URL url = new URL("https://raw.githubusercontent.com/Yurizaki/caixiu-resources/main/vocabulary.csv");
-        BufferedInputStream in = new BufferedInputStream(url.openStream());
-
-        ReadableByteChannel readableByteChannel = Channels.newChannel(url.openStream());
-        FileOutputStream fileOutputStream = new FileOutputStream("tempFile.csv");
-        fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
-
-
-        s3Client.putObject(PutObjectRequest.builder().bucket("caixiu-dev").key("vocabulary.csv").build(),
-                RequestBody.fromBytes(getObjectFile("tempFile.csv")));
+        this.bucketName = bucketName;
     }
 
-    private static byte[] getObjectFile(String filePath) {
+    public PutObjectResponse putFile(String fileKey, byte[] objectToPut) {
+        return s3Client.putObject(PutObjectRequest.builder().bucket(bucketName).key(fileKey).build(),
+                RequestBody.fromBytes(objectToPut));
+    }
 
-        FileInputStream fileInputStream = null;
-        byte[] bytesArray = null;
+    public File getFileByKey(String fileKey) {
+        ResponseBytes<GetObjectResponse> getFileResponse = s3Client.getObjectAsBytes(
+                GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileKey)
+                .build());
+
+        byte[] bb = getFileResponse.asByteArray();
+        File file = new File(fileKey);
 
         try {
-            File file = new File(filePath);
-            bytesArray = new byte[(int) file.length()];
-            fileInputStream = new FileInputStream(file);
-            fileInputStream.read(bytesArray);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (fileInputStream != null) {
-                try {
-                    fileInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+            OutputStream outputStream = Files.newOutputStream(file.toPath());
+            outputStream.write(bb);
         }
-        return bytesArray;
+        catch (IOException ex) {
+        }
+
+        return file;
     }
 }
--- src/test/java/com/holmes/aws/vocabulary/AwsS3VocabularyTest.java
+++ src/test/java/com/holmes/aws/vocabulary/AwsS3VocabularyTest.java
@@ -6,14 +6,26 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
-import software.amazon.awssdk.services.dynamodb.waiters.DynamoDbWaiter;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.S3ClientBuilder;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class AwsS3VocabularyTest {
@@ -21,10 +33,17 @@ class AwsS3VocabularyTest {
     private static S3Client s3Client;
     private static AwsS3Vocabulary awsS3Vocabulary;
 
+    private static final String BUCKET_NAME = "caixiu-dev";
+    private static final String FILE_NAME = "vocabulary.csv";
+    private static URL fileUrl;
+
+
     @BeforeAll
     static void setup() throws IOException {
         s3Client = S3Client.builder().region(Region.EU_WEST_2).build();
-        awsS3Vocabulary = new AwsS3Vocabulary(s3Client);
+        awsS3Vocabulary = new AwsS3Vocabulary(s3Client, BUCKET_NAME);
+
+        fileUrl = new URL("https://raw.githubusercontent.com/Yurizaki/caixiu-resources/main/vocabulary.csv");
     }
 
     @AfterAll
@@ -32,9 +51,60 @@ class AwsS3VocabularyTest {
         s3Client.close();
     }
 
+    @Test
+    public void putFile_pass_if_file_added() throws IOException {
+        BufferedInputStream in = new BufferedInputStream(fileUrl.openStream());
+        ReadableByteChannel readableByteChannel = Channels.newChannel(fileUrl.openStream());
+        FileOutputStream fileOutputStream = new FileOutputStream("tempFile.csv");
+        fileOutputStream.getChannel().transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+
+        awsS3Vocabulary.putFile(FILE_NAME, getObjectFile("tempFile.csv"));
+    }
+
+    private static byte[] getObjectFile(String filePath) {
+        FileInputStream fileInputStream = null;
+        byte[] bytesArray = null;
+
+        try {
+            File file = new File(filePath);
+            bytesArray = new byte[(int) file.length()];
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bytesArray);
+
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null) {
+                try {
+                    fileInputStream.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return bytesArray;
+    }
+
 
     @Test
-    public void testy() {
+    public void getFileFromS3Bucket_pass_if_file_retrieved() throws FileNotFoundException {
+        File myVocabFile = awsS3Vocabulary.getFileByKey(FILE_NAME);
 
+        assertNotNull(myVocabFile);
+        assertEquals(FILE_NAME, myVocabFile.getName());
+
+        List<Vocabulary> vocabularyList = new ArrayList<>();
+        Scanner scanner = new Scanner(myVocabFile);
+        while(scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+
+            String[] splitter = line.split(",");
+            vocabularyList.add(new Vocabulary(splitter[0], splitter[1]));
+        }
+
+        assertTrue(vocabularyList.size() > 0);
     }
 }
